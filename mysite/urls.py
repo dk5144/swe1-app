04edbcf7ebd33b97efc8c94129d548b272eeaa2c
Dhrity Kachhwaha <dhritykachhwaha@dhritys-air.mynetworksettings.com>
2025-10-05 21:54:56 -0400

    Redirect site root to /admin/

--- mysite/urls.py
+++ mysite/urls.py
@@ -23,5 +23,5 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
-    path("", RedirectView.as_view(url="/polls/", permanent=False)),
+    path("", RedirectView.as_view(url="/admin/", permanent=False)),
 ]
