f630036b8ff7bd764424a7e1f99ea9c9b335c9c4
Dhrity Kachhwaha <dhritykachhwaha@dhritys-air.mynetworksettings.com>
2025-10-19 17:48:17 -0400

    updated test cases

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
 # Create your tests here.
+import datetime
+from django.utils import timezone
+from django.test import TestCase
+from polls.models import Question
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_future(self):
+        future = timezone.now() + datetime.timedelta(days=1)
+        q = Question.objects.create(question_text="f", pub_date=future)
+        self.assertFalse(q.was_published_recently())
+
+    def test_was_published_recently_old(self):
+        old = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        q = Question.objects.create(question_text="o", pub_date=old)
+        self.assertFalse(q.was_published_recently())
+
+    def test_was_published_recently_recent(self):
+        recent = timezone.now() - datetime.timedelta(hours=23, minutes=59)
+        q = Question.objects.create(question_text="r", pub_date=recent)
+        self.assertTrue(q.was_published_recently())
